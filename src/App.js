92bc02921a87f0ca560c237d87c7e737904c1438
dheroju <dherry007@gmail.com>
2022-07-18 22:14:33 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Aderoju Johnson and is{" "}
-        <a
-          href="https://github.com/dheroju/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Aderoju Johnson and is{" "}
+          <a
+            href="https://github.com/dheroju/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
