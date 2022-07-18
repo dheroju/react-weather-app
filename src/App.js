cd7d455e4cb2280d2f785b02aafc085e31daf3b8
dheroju <dherry007@gmail.com>
2022-07-18 14:05:00 +0100

    Added the GIthub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
+import react from "react";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Aderoju Johnson and is{" "}
+        <a href="https://github.com/dheroju/react-weather-app" target="_blank">
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
