c76c36c6488085e0016cbeb604463c40ae272e83
dheroju <dherry007@gmail.com>
2022-07-19 23:27:32 +0100

    Added  Api integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultcity="Lagos" />
         <footer>
           This project was coded by Aderoju Johnson and is{" "}
           <a
