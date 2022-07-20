55968e0f139b499839c3e269b9191a021cd743c4
dheroju <dherry007@gmail.com>
2022-07-20 02:03:36 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
           <a
             href="https://github.com/dheroju/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced
           </a>
