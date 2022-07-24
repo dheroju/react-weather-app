1b55880fc2ff21bdfb5d863e13ea20fb3333d1ee
dheroju <dherry007@gmail.com>
2022-07-24 01:01:12 +0100

    Worked on the weather search Engine component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultcity="Lagos" />
+        <Weather defaultCity="Lagos" />
         <footer>
           This project was coded by Aderoju Johnson and is{" "}
           <a
