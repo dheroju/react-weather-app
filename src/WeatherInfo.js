189a55647e2f8d919c5acebe9b5cb40111ea83b0
dheroju <dherry007@gmail.com>
2022-07-24 16:11:16 +0100

    Updated icon in weather app

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,6 +14,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
+            <WeatherIcon code={props.data.icon} />
             <img
               src={props.data.iconUrl}
               alt={props.data.description}
