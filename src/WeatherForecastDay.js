27bc6f9387a0b04e3b13b108e646102dc8126ad0
dheroju <dherry007@gmail.com>
2022-07-31 23:41:38 +0100

    Finishing touches to css weather elements

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -25,12 +25,8 @@ export default function WeatherForecastDay(props) {
       <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
-        <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}
-        </span>
-        <span className="WeatherForecast-temperature-min">
-          {minTemperature()}
-        </span>
+        <span className="Forecast-temperature-max">{maxTemperature()}</span>
+        <span className="Forecast-temperature-min">{minTemperature()}</span>
       </div>
     </div>
   );
